3fed59a0c896a72f9ab3127545d3ca4c71852387
Darren Hulipas <darrenhulipas@gmail.com>
2024-10-02 23:29:00 +0200

    Update design

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'package:flutter_fitness_time/widget/bottomnavigationbar.dart';
-
-
-
 void main() {
   runApp(const MainApp());
-  void main() => runApp(const NavigationBarApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -16,32 +11,27 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.montserrat().fontFamily,
+        appBarTheme: const AppBarTheme(
+          foregroundColor: Colors.white,
+          backgroundColor: Color(0xFFF896D8),
+          centerTitle: true,
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color(0xFFF896D8),
-          titleTextStyle: GoogleFonts.montserrat(),
-          title: Text('Fitness Time',style: TextStyle(color: Colors.white),),
-          actions: <Widget>[
-            IconButton(
-              //Foto de perfil (Añadir CircleAvatar, NetworkImage)
-              icon: const Icon(Icons.person),
-              tooltip: 'Profile pic',
-              onPressed: () {
-                //Entrar perfil
-              },
+          title: Text(
+            'Fitness Time',
+          ),
+        ),
+        body: Column(
+          children: [
+            Text(
+              "Hola Antònia,",
             ),
           ],
         ),
-        body: const Center(
-          /**
-           * Título + texto + link
-           * 
-           * Card  + CardPercentIndicator -> percent_indicator 
-           */
-          child: Text('Hello World!'),
-          
-        ),
-        
       ),
     );
   }
---
+++ lib/widget/singlechildscrollview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Singlechildscrollview extends StatelessWidget {
+  const Singlechildscrollview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(
+      children: [],
+    ));
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
